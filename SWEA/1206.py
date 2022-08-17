2e6fec145ad3386edb9918eeb57695b8dcf0fd17
Song-d-s <s2770853@gmail.com>
2022-08-17 21:22:56 +0900

    22/08/08 View

--- SWEA/1206.py
+++ SWEA/1206.py
@@ -2,7 +2,7 @@
 import sys
 sys.stdin = open("input.txt")
 T = 10
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+#
 for test_case in range(1, T + 1):
     N = int(input())
     buildings = list(map(int, list(input().split())))
